1947f01412dede0784ed9d0923cfb864e28402a6
Maria <ma.alampay@gmail.com>
2025-01-10 11:20:54 -0600

    Made some more dialogue in my Restaurant class :)

---
+++ src/Menu.java
@@ -0,0 +1,15 @@
+/*
+ * author: Maria Alampay
+ * date: 10/01/2025
+ * Menu class 
+ */
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int burger = input.nextInt();
+        
+    }
+}
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,21 +1,36 @@
 /*
  * author: Maria Alampay
- * date: 10/09/2025
+ * date: 09/01/2025
  * Restaurant class 
  */
 
  import java.util.Scanner;
+ import java.util.Random;
 
 public class Restaurant {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Restaurant, how many people are in your party?"); // will change the name later but for now it is 'Restaurant'
-        int num_of_people_in_party = input.nextInt();
+        
+        String inp = input.nextLine();
+        if (inp.equals("exit")) {
+            System.out.println("Goodbye!");
+            return;
+        }
+        int num_of_people_in_party = Integer.parseInt(inp);
         System.out.println("Great! Please be seated at Table 4, I'll bring out the menu."); // can I generate random numbers for tables between 1-14??? (maybe)
         
+        inp = input.nextLine();
+        if(inp.equals("ready")){
+            System.out.println("What appetizer would you like to start with?");
+        }
         String Goodbye = input.nextLine();
-        if(Goodbye == "exit"){
+        if(Goodbye.equals("exit")){
             System.out.println("Goodbye!");
-        } //something is wrong here and I will fix this tommorow 
+        }
+        // this ^^^ still doesnt work but ill fix it later
+
+
+        
     }
 }  
